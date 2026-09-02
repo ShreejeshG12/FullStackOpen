6ff7e346a61189dcd135161aada441a6a76a158d
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-09-01 22:45:15 -0400

    Submitting Unicafe assignment step 1

---
+++ part_1/Unicafe/step_1/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.title}</h1>
+    </>
+  )
+}
+
+const Result = (props) => {
+  return (
+    <p>{props.name} {props.clicks}</p>
+  )
+}
+
+const App = () => {
+
+  const [reviews, setReviews] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+
+
+  const handleGoodClick = () => {
+    console.log("Good clicked")
+    const newReview = {
+      ...reviews,
+      good: reviews.good + 1
+    }
+    setReviews(newReview)
+  }
+
+  const handleNeutralClick = () => {
+    console.log("Neutral clicked")
+    const newReview = {
+      ...reviews,
+      neutral: reviews.neutral + 1
+    }
+    setReviews(newReview)
+  }
+
+  const handleBadClick = () => {
+    console.log("Bad clicked")
+    const newReview = {
+      ...reviews,
+      bad: reviews.bad + 1
+    }
+    setReviews(newReview)
+  }
+
+
+  return (
+    <div>
+
+      <Header title={"give feedback"} />
+      <Button onClick={handleGoodClick} text={"good"} />
+      <Button onClick={handleNeutralClick} text={"neutral"} />
+      <Button onClick={handleBadClick} text={"bad"} />
+
+      <Header title={"statistics"} />
+
+      <Result name={"good"} clicks={reviews.good} />
+      <Result name={"neutral"} clicks={reviews.neutral} />
+      <Result name={"bad"} clicks={reviews.bad} />
+
+
+    </div>
+  )
+
+
+}
+
+
+export default App
