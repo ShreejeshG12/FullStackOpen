1a2a95580cbfe06c3f2dfd9decac5dcbe57aa112
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-09-02 00:27:32 -0400

    Submission Unicafe Step 4

--- part_1/Unicafe/step_1/src/App.jsx
+++ part_1/Unicafe/step_1/src/App.jsx
@@ -16,7 +16,7 @@ const Result = (props) => {
   )
 }
 
-const Stats = (props) => {
+const Statistics = (props) => {
   return (
     <p>{props.name} {props.nums}</p>
   )
@@ -57,7 +57,10 @@ const App = () => {
   }
 
 
+
   return (
+
+
     <div>
 
       <Header title={"give feedback"} />
@@ -67,14 +70,21 @@ const App = () => {
 
       <Header title={"statistics"} />
 
-      <Result name={"good"} clicks={reviews.good} />
-      <Result name={"neutral"} clicks={reviews.neutral} />
-      <Result name={"bad"} clicks={reviews.bad} />
-      <Stats name={"all"} nums={reviews.good + reviews.bad + reviews.neutral} />
-      <Stats name={"average"} nums={((reviews.good - reviews.bad) / (reviews.neutral + reviews.good + reviews.bad))} />
-      <Stats name={"positive"} nums={`${(reviews.good / (reviews.good + reviews.bad + reviews.neutral)) * 100}%`} />
+      {reviews.good + reviews.bad + reviews.neutral > 0 ? (
+        <>
+          <Result name={"good"} clicks={reviews.good} />
+          <Result name={"neutral"} clicks={reviews.neutral} />
+          <Result name={"bad"} clicks={reviews.bad} />
+          <Statistics name={"all"} nums={reviews.good + reviews.bad + reviews.neutral} />
+          <Statistics name={"average"} nums={((reviews.good - reviews.bad) / (reviews.neutral + reviews.good + reviews.bad))} />
+          <Statistics name={"positive"} nums={`${(reviews.good / (reviews.good + reviews.bad + reviews.neutral)) * 100}%`} />
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   )
+
 
 
 }
