6adfed4871d8caeefca44a6c247e40f6b038c2cb
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-09-02 22:38:31 -0400

    Submitting assignment unicafe step 5

--- part_1/Unicafe/step_1/src/App.jsx
+++ part_1/Unicafe/step_1/src/App.jsx
@@ -10,15 +10,12 @@ const Header = (props) => {
   )
 }
 
-const Result = (props) => {
+const StatisticsLines = (props) => {
   return (
-    <p>{props.name} {props.clicks}</p>
-  )
-}
-
-const Statistics = (props) => {
-  return (
-    <p>{props.name} {props.nums}</p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -27,7 +24,6 @@ const App = () => {
   const [reviews, setReviews] = useState({
     good: 0, neutral: 0, bad: 0
   })
-
 
   const handleGoodClick = () => {
     console.log("Good clicked")
@@ -55,12 +51,7 @@ const App = () => {
     }
     setReviews(newReview)
   }
-
-
-
   return (
-
-
     <div>
 
       <Header title={"give feedback"} />
@@ -71,22 +62,21 @@ const App = () => {
       <Header title={"statistics"} />
 
       {reviews.good + reviews.bad + reviews.neutral > 0 ? (
-        <>
-          <Result name={"good"} clicks={reviews.good} />
-          <Result name={"neutral"} clicks={reviews.neutral} />
-          <Result name={"bad"} clicks={reviews.bad} />
-          <Statistics name={"all"} nums={reviews.good + reviews.bad + reviews.neutral} />
-          <Statistics name={"average"} nums={((reviews.good - reviews.bad) / (reviews.neutral + reviews.good + reviews.bad))} />
-          <Statistics name={"positive"} nums={`${(reviews.good / (reviews.good + reviews.bad + reviews.neutral)) * 100}%`} />
-        </>
+        <table>
+          <tbody>
+            <StatisticsLines text={"good"} value={reviews.good} />
+            <StatisticsLines text={"neutral"} value={reviews.neutral} />
+            <StatisticsLines text={"bad"} value={reviews.bad} />
+            <StatisticsLines text={"all"} value={reviews.good + reviews.bad + reviews.neutral} />
+            <StatisticsLines text={"average"} value={((reviews.good - reviews.bad) / (reviews.neutral + reviews.good + reviews.bad))} />
+            <StatisticsLines text={"positive"} value={`${(reviews.good / (reviews.good + reviews.bad + reviews.neutral)) * 100}%`} />
+          </tbody>
+        </table>
       ) : (
         <p>No feedback given</p>
       )}
     </div>
   )
-
-
-
 }
 
 
