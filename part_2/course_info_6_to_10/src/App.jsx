54f5290d8860c003d8347b46a35a060d5594c4aa
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-09-12 23:05:58 -0400

    Submitting Part 2 excercises step 6 & 7

---
+++ part_2/course_info_6_to_10/src/App.jsx
@@ -0,0 +1,68 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Course = (props) => {
+  const { course } = props
+  return (
+    course.parts.map((item, i) => {
+      return <p key={i}>{item.name} {item.exercises}</p>
+    })
+  )
+}
+
+const Total = (props) => {
+  const { course } = props
+  const total = course.parts.reduce((sum, part) => {
+    return sum + part.exercises
+  }, 0)
+  return (
+    <h2>Total of {total} exercises</h2>
+  )
+
+}
+
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Course course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default App
