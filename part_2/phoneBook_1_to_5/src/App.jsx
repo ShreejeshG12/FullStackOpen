c074eee0ff11bcc2f410b997507496815362d856
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-09-13 22:05:37 -0400

    Submission assignment phonebook Step 1

---
+++ part_2/phoneBook_1_to_5/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+
+const Person = (props) => {
+  return (
+    <li>{props.name}</li>
+  )
+}
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", id: "1" }
+  ])
+  const [newName, setNewName] = useState("")
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: String(persons.length + 1)
+    }
+    setPersons(persons.concat(personObject))
+    setNewName("")
+  }
+
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName}
+            onChange={event => setNewName(event.target.value)}
+          />
+
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <Person key={person.id} name={person.name} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
