b9f1fba4fb0748e8297f9d8333effe9bad120f4a
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-09-13 22:42:20 -0400

    Submission assignment phonebook step 3

--- part_2/phoneBook_1_to_5/src/App.jsx
+++ part_2/phoneBook_1_to_5/src/App.jsx
@@ -3,26 +3,29 @@ import { useState } from "react";
 
 const Person = (props) => {
   return (
-    <li>{props.name}</li>
+    <li>{props.name} {props.number}</li>
   )
 }
 
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", id: "1" }
+    { name: "Arto Hellas", id: "1", number: "123 - 445 - 678" }
   ])
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
   const addPerson = (event) => {
     event.preventDefault()
     if (!samePerson) {
       const personObject = {
         name: newName,
-        id: String(persons.length + 1)
+        id: String(persons.length + 1),
+        number: String(newNumber)
       }
       setPersons(persons.concat(personObject))
       setNewName("")
+      setNewNumber("")
     }
     else {
       alert(`${newName} is already added to phonebook`)
@@ -30,6 +33,7 @@ const App = () => {
     }
 
   }
+
 
   const samePerson = persons.find(person =>
     person.name.toLowerCase() === newName.toLowerCase()
@@ -44,7 +48,10 @@ const App = () => {
           name: <input value={newName}
             onChange={event => setNewName(event.target.value)}
           />
-
+          <br />
+          number: <input value={newNumber}
+            onChange={event => setNewNumber(event.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -53,7 +60,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <Person key={person.id} name={person.name} />
+          <Person key={person.id} name={person.name} number={person.number} />
         )}
       </ul>
     </div>
